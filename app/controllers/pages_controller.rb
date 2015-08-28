d10e5ca42a75e8e5fa5b484ee8341a5e1ffb3f33
VoroninNick <p.korenev@voroninstudio.eu>
2015-08-28 17:38:34 +0300

    phones

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def stub
-    @phones = ["+7 7172 539 277", "+7 7172 539 277", "+7 7172 539 277", "+7 7172 539 277"]
+    @phones = ["+7 7172 539 277", "+7 7172 539 275", "+7 7273 30 2323", "+7 7272 59 6263"]
     @mail = "info@orislegal.com"
   end
 end
