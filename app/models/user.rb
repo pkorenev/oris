425f2baeb50be4d5bf3af922f338fac04984ac07
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-01 23:27:13 +0200

    user login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,# :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  attr_accessible *attribute_names
+
+  attr_accessible :password, :password_confirmation
 end
