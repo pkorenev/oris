16d4c9b3b39f685d7b19eb0fec159ebc903d4fcc
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2015-11-05 02:00:32 +0200

    Production configs, and deploy shared files/dirs oris specific configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,18 @@ set :repo_url, 'git@github.com:yasinishyn/oris-1.git'
 # how many old releases do we want to keep, not much
 set :keep_releases, 5
 
+
+# puma settings
+set :puma_init_active_record, true
+
+# Don't change these unless you know what you're doing
+set :pty, true
+set :forward_agent, true
+set :sudo_prompt, 'qwwppqjwfjpqej220220349__0010203002lsdfsldkfjlkjjjqjdeployer'
 # files we want symlinking to specific entries in shared
 set(:linked_files, %w(
   config/database.yml
-  puma.rb
   config/secrets.yml
-  config/application.yml
 ))
 
 set(:linked_dirs, %w(
@@ -25,15 +31,17 @@ set(:linked_dirs, %w(
   vendor/bundle
   public/system
   public/uploads
+  db
 ))
 
 set(:config_files, %w(
   nginx.conf
   database.example.yml
   log_rotation
+  secrets.yml
 ))
 
-set :ssh_options, port: 136_66,
+set :ssh_options, port: 22,
                   forward_agent: true,
                   user: fetch(:user),
                   keys: %w(~/.ssh/id_rsa.pub)
@@ -48,21 +56,12 @@ set(:symlinks, [
   }
 ])
 
-# puma settings
-set :puma_init_active_record, true
-
-# Don't change these unless you know what you're doing
-set :pty,        true
-set :use_sudo,   false
-set :deploy_via, :remote_cache
-
 namespace :deploy do
   # make sure we're deploying what we think we're deploying
   before :deploy, 'deploy:check_revision'
   # only allow a deploy with passing tests to deployed
   # before :deploy, "deploy:run_tests"
   # compile assets locally then rsync
-  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
   after :finishing, 'deploy:cleanup'
 
   # remove the default nginx configuration as it will tend
@@ -72,10 +71,6 @@ namespace :deploy do
   # reload nginx to it will pick up any modified vhosts from
   # setup_config
   after 'deploy:setup_config', 'nginx:reload'
-
-  # Restart monit so it will pick up any monit configurations
-  # we've added
-  # after 'deploy:setup_config', 'monit:restart'
 
   # As of Capistrano 3.1, the `deploy:restart` task is not called
   # automatically.
