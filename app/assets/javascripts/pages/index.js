c04f671e4592c72fbd626a4225737c1ec5566e6d
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-05 09:59:53 +0200

    fix js error

--- app/assets/javascripts/pages/index.js
+++ app/assets/javascripts/pages/index.js
@@ -1,22 +1,26 @@
 (function($){
     $(document).ready(function(){
 
+
         //bxslider
         $('#slide-counter').prepend('<span class="current-index"></span> / ');
 
-        var slider = $('.bx_px').bxSlider({
-            auto: true,
-            pager: false,
-            onSliderLoad: function (currentIndex){
-                $('#slide-counter .current-index').text(currentIndex + 1);
-            },
-            onSlideBefore: function ($slideElement, oldIndex, newIndex){
-                $('#slide-counter .current-index').text(newIndex + 1);
-            }
-        });
+        var $bx_px = $('.bx_px')
 
-        $('#slide-counter').append(slider.getSlideCount());
+        if ($bx_px.length) {
+            var slider = $('.bx_px').bxSlider({
+                auto: true,
+                pager: false,
+                onSliderLoad: function (currentIndex) {
+                    $('#slide-counter .current-index').text(currentIndex + 1);
+                },
+                onSlideBefore: function ($slideElement, oldIndex, newIndex) {
+                    $('#slide-counter .current-index').text(newIndex + 1);
+                }
+            });
 
+            $('#slide-counter').append(slider.getSlideCount());
+        }
     });
 
 })(jQuery);
